1db28bf42bc09cf85d8bd2d1699212fdd3e6d9a7
M Aswin Kishore <mak26.work@gmail.com>
2021-12-13 02:25:09 +0530

    add: param disable: boolean to command files

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -7,6 +7,6 @@ module.exports = (client) => {
     const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
     commandFiles.forEach((file) => {
         const command = require(`../commands/${file}`);
-        client.commands.set(command.name, command);
+        if (!command.disable) client.commands.set(command.name, command);
     });
 };
