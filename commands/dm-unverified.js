4d395efbc67c62e7cbdcd6a1a2f6d1069626b83f
M Aswin Kishore <mak26.work@gmail.com>
2021-12-27 16:58:46 +0530

    fix: bot spam link

--- commands/dm-unverified.js
+++ commands/dm-unverified.js
@@ -34,7 +34,7 @@ module.exports = {
                     .setDescription(
                         [
                             `Use command \`${PREFIX}verify-me\` in **bot-spam** channel of server`,
-                            '\nChannel Link: https://discord.com/channels/914867137863118919/914867138668404766',
+                            '\nChannel Link: https://discord.com/channels/914867137863118919/924977270739836978',
                         ].join('\n')
                     )
                     .setFooter('Wish to see you there soon🙌');
